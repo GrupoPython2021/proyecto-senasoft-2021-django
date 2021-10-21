cecfe03afe5877fc4d40afbe498569a401652b9f
Cristian A. Ariza R <90012640+AxOrSha2@users.noreply.github.com>
2021-10-21 15:03:06 -0500

    commit

--- ssoft2021/applications/sesion/views.py
+++ ssoft2021/applications/sesion/views.py
@@ -30,10 +30,6 @@ class CrearSalaView(FormView):
 
     casa={'1':'casa'}
 
-    env = Environment(loader=FileSystemLoader("C:\\Users\\mwmar\\Desktop\\ss\\proyecto-senasoft-2021-django\\ssoft2021\\templates\\cartas"))
-    template = env.get_template("iniciar-partida.html")
-    html = template.render(casa)
-
 
 
 
