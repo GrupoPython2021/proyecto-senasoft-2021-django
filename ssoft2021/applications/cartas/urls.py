d0424a032f6c416c87ffc51acb1c7394c9665759
Cristian A. Ariza R <90012640+AxOrSha2@users.noreply.github.com>
2021-10-22 12:37:10 -0500

    Actualizado el template

--- ssoft2021/applications/cartas/urls.py
+++ ssoft2021/applications/cartas/urls.py
@@ -7,10 +7,29 @@ apps_name="cartas_app"
 
 
 urlpatterns = [
-    path('inicio-partida/', views.IniciarPartida),
+    path(
+        'inicio-partida/', 
+        views.IniciarPartida
+    ),
     # path('duplicar/', views.duplicar_lista),
-    path('barajar-sistema/', views.barajar_sistema),
+    path(
+        'barajar-sistema/', 
+        views.barajar_sistema
+    ),
     #
-    path('retomar_partida/', views.Retomar_Partida, name='retomar'),
-    path('unirse_sesion/', views.Unirse_Sesion, name='unirse'),
+    path(
+        'retomar_partida/', 
+        views.Retomar_Partida, 
+        name='retomar'
+    ),
+    path(
+        'unirse_sesion/', 
+        views.Unirse_Sesion, 
+        name='unirse'
+    ),
+    path(
+        'modal_acusar/',
+        views.modal_acusar.as_view(),
+        name='modal-acusar'
+    ),
 ]
